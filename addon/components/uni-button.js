0ac20a6f623420e1a77b954ad179eb3071a71c03
André Joaquim <andrej_21@hotmail.com>
2016-10-25 15:05:19 +0100

    Add isDisabled flag to uni-button

--- addon/components/uni-button.js
+++ addon/components/uni-button.js
@@ -3,9 +3,10 @@ import layout from '../templates/components/uni-button';
 
 export default Ember.Component.extend({
   tagName: 'button',
-  classNameBindings: ['isPrimary:uni-button--primary'],
+  classNameBindings: ['isPrimary:uni-button--primary', 'isDisabled:uni-button--disabled'],
   classNames: ['uni-button'],
   layout,
 
-  isPrimary: false
+  isPrimary: false,
+  isDisabled: false
 });
