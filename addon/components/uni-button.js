6ff62847e13e7e25311630429b391f46983c68fb
André Joaquim <andrej_21@hotmail.com>
2016-10-27 10:59:18 +0100

    Added isSecondary as well

--- addon/components/uni-button.js
+++ addon/components/uni-button.js
@@ -3,10 +3,14 @@ import layout from '../templates/components/uni-button';
 
 export default Ember.Component.extend({
   tagName: 'button',
-  classNameBindings: ['isPrimary:uni-button--primary', 'isDisabled:uni-button--disabled'],
+  classNameBindings: [
+    'isPrimary:uni-button--primary',
+    'isDisabled:uni-button--disabled',
+    'isSecondary:uni-button--secondary'],
   classNames: ['uni-button'],
   layout,
 
   isPrimary: false,
-  isDisabled: false
+  isDisabled: false,
+  isSecondary: false
 });
