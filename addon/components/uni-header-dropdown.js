b4248f9450189f3c9dd24c21cda715f46e1f7f72
Miguel Melo <miguelbritomelo@gmail.com>
2016-11-22 18:15:14 +0000

    further fixes to previous implementation

--- addon/components/uni-header-dropdown.js
+++ addon/components/uni-header-dropdown.js
@@ -4,11 +4,16 @@ import layout from '../templates/components/uni-header-dropdown';
 const { Component } = Ember;
 
 export default Component.extend({
-  isOpen: false,
   tagName: '',
   layout,
+  isOpen: false,
+  componentName: 'uni-header-dropdown-item-simple',
+
+  switch() {},
 
   actions: {
-    switch() {}
+    switch() {
+      this.get('switch')();
+    }
   }
 });
--- addon/components/uni-header-menu.js
+++ addon/components/uni-header-menu.js
@@ -1,20 +1,38 @@
 import Ember from 'ember';
 import layout from '../templates/components/uni-header-menu';
 import headerButtons from '../enums/header-buttons';
-import headerOptions from '../enums/header-menu';
 import languages from '../enums/languages';
 
 const { Component } = Ember;
 
+const headerOptions = [
+  {
+    label: 'Account',
+    route: '/account' 
+  },
+  {
+    label: 'Billing',
+    route: '/billing'
+  },
+  {
+    label: 'Logout',
+    route: '/logout'
+  }
+];
+
 export default Component.extend({
   headerButtons: headerButtons.values(),
-  headerOptions: headerOptions.values(),
+  headerOptions: headerOptions,
   languageOptions: languages.values(),
   exitLabel: 'Exit',
   tagName: '',
   layout,
 
+  triggerExit() {},
+
   actions: {
-    triggerExit() {}
+      triggerExit() {
+        this.get('triggerExit')();
+      }
   }
 });
