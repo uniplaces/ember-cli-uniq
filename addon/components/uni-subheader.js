3cdf0368b5d6ea3736812ddbf4e92f80e6fa7766
mmelo <mmelo@users.noreply.github.com>
2016-11-24 10:49:11 +0000

    Update uni-subheader.js (#39)

--- addon/components/uni-subheader.js
+++ addon/components/uni-subheader.js
@@ -4,7 +4,6 @@ import layout from '../templates/components/uni-subheader';
 const { Component } = Ember;
 
 export default Component.extend({
-  tagName: '',
   classNames: ['uni-subheader'],
   layout
 });
