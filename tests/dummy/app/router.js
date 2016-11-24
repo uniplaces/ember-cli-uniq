5f8ea614a734ead5ce9e3d429048331c31d9251e
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2016-11-24 00:32:33 +0000

    Fix all eslint errors

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+const { Router } = Ember;
+
+const EmberRouter = Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
 });
 
-export default Router;
+export default EmberRouter;
