3ad7a29d45a27bc9ed38cef7a6533aa1b4fcb97e
André Joaquim <andrej_21@hotmail.com>
2016-11-24 16:40:01 +0000

    Add uni-select and improve phone number dropdown

--- addon/components/uni-mobile-number.js
+++ addon/components/uni-mobile-number.js
@@ -3,13 +3,13 @@ import layout from '../templates/components/uni-mobile-number';
 
 const { Component } = Ember;
 const options = [
-  { language: 'de', country_code: '+40' },
-  { language: 'fr', country_code: '+33' },
-  { language: 'es', country_code: '+35' },
-  { language: 'it', country_code: '+39' },
-  { language: 'uk', country_code: '+44' },
-  { language: 'pt', country_code: '+351' },
-  { language: 'be', country_code: '+32' }
+  { language: 'Germany', country_code: '+40' },
+  { language: 'France', country_code: '+33' },
+  { language: 'Spain', country_code: '+35' },
+  { language: 'Italy', country_code: '+39' },
+  { language: 'United Kingdom', country_code: '+44' },
+  { language: 'Portugal', country_code: '+351' },
+  { language: 'Belgium', country_code: '+32' }
 ];
 
 export default Component.extend({
