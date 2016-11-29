9abd5132ed654a9a6a44488b6816c6104330f134
André Joaquim <andrej_21@hotmail.com>
2016-11-29 15:51:56 +0000

    Refactor uni-header and migrate puzzling to SS

--- addon/components/uni-header-menu.js
+++ addon/components/uni-header-menu.js
@@ -1,23 +1,9 @@
 import Ember from 'ember';
 import layout from '../templates/components/uni-header-menu';
-import Languages from '../enums/languages';
 
 const { Component } = Ember;
-const PLACEHOLDER_USERNAME = 'Uniprovider';
-const PLACEHOLDER_LANGUAGE = 'En';
 
 export default Component.extend({
   tagName: '',
-  layout,
-  languages: Languages.toKeyValueJson(),
-  isUserLoggedIn: false,
-  language: PLACEHOLDER_LANGUAGE,
-  username: PLACEHOLDER_USERNAME,
-
-  actions: {
-    onLanguageBtnClick(countryCode) {
-      console.log(countryCode);
-      console.log('clicked language btn');
-    }
-  }
+  layout
 });
