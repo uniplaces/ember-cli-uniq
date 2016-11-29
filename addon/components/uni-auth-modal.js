263f8d31d4542dd77c9ee5d3bd02eaf142e90bf9
Miguel Melo <miguelbritomelo@gmail.com>
2016-11-29 17:24:47 +0000

    minor fixed based on provided feedback

---
+++ addon/components/uni-auth-modal.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import layout from '../templates/components/uni-auth-modal';
+
+const { Component } = Ember;
+
+export default Component.extend({
+  tagName: '',
+  layout,
+  isOpen: false
+});
