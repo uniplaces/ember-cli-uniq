e6f3ded147367dd4c0b284c81c225e7e4ad73aa2
mmelo <mmelo@users.noreply.github.com>
2016-11-30 19:04:08 +0000

    added sign in component (#48)

---
+++ addon/components/uni-signin.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import layout from '../templates/components/uni-signin';
+
+const { Component } = Ember;
+
+export default Component.extend({
+  classNames: ['uni-signin'],
+  layout
+});
