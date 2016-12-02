724690ea6ee175a742029dd54447f261e7051eeb
André Joaquim <andrej_21@hotmail.com>
2016-12-02 13:01:53 +0000

    Refactor uni-step-card

--- tests/integration/components/uni-alert-test.js
+++ tests/integration/components/uni-alert-test.js
@@ -6,19 +6,9 @@ moduleForComponent('uni-alert', 'Integration | Component | uni alert', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(1);
 
   this.render(hbs`{{uni-alert}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#uni-alert}}
-      template block text
-    {{/uni-alert}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.notEqual(this.$().text().trim(), '');
 });
