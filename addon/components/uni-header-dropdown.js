462e7cd27c23240773ae338c75c1d03944ed66bc
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2016-12-06 22:33:00 +0000

    Close on click outside

--- addon/components/uni-header-dropdown.js
+++ addon/components/uni-header-dropdown.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 import layout from '../templates/components/uni-header-dropdown';
-
+import ClickOutsideMixin from 'ember-cli-uniq/mixins/click-outside';
 const { Component } = Ember;
 
-export default Component.extend({
-  tagName: '',
+export default Component.extend(ClickOutsideMixin, {
+  tagName: 'span',
   layout,
   isOpen: false,
   componentName: 'uni-header-dropdown-item-simple',
@@ -13,7 +13,13 @@ export default Component.extend({
 
   actions: {
     switch() {
+      this.set('isOpen', false);
+
       this.get('switch')();
     }
+  },
+
+  onOutsideClick() {
+    this.set('isOpen', false);
   }
 });
