36a80c9ea032fd6bd78927266de69e94a5b08d0a
mmelo <mmelo@users.noreply.github.com>
2016-12-06 01:22:27 +0000

    [RFC][UN-7148] Tabs if the user goes back - ember components (#52)
    * created uni-tabs component
    
    * fixed previous implementation based on feedback

---
+++ addon/components/uni-tabs.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import layout from '../templates/components/uni-tabs';
+
+const { Component } = Ember;
+
+export default Component.extend({
+  classNames: ['uni-tabs'],
+  layout
+});
---
+++ tests/integration/components/uni-tabs-test.js
@@ -0,0 +1,30 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+const DEFAULT_LABEL = 'uni tab label';
+
+moduleForComponent('uni-tabs', 'Integration | Component | uni tabs', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`{{uni-tabs}}`);
+
+  assert.equal(this.$().text().trim(), '');
+});
+
+test('it renders and renders items', function(assert) {
+  assert.expect(2);
+
+  this.set('tabs', [{
+    label: DEFAULT_LABEL,
+    url: ''
+  }]);
+
+  this.render(hbs`{{uni-tabs tabs=tabs}}`);
+
+  assert.notEqual(this.$().text().trim(), '');
+  assert.equal(this.$('.uni-tabs__wrapper__item a').text().trim(), DEFAULT_LABEL);
+});
