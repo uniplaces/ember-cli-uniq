c4c596fb15220a37a9bc86850226f5e03c126110
Miguel Melo <miguelbritomelo@gmail.com>
2016-12-07 18:41:12 +0000

    made requested changes

--- addon/components/uni-carrousel.js
+++ addon/components/uni-carrousel.js
@@ -1,54 +1,35 @@
 import Ember from 'ember';
 import layout from '../templates/components/uni-carrousel';
 
-const { Component, $ } = Ember;
+const { Component, computed, $ } = Ember;
 
 export default Component.extend({
   classNames: ['uni-carrousel'],
   layout,
   page: 0,
   componentWidth: 0,
-  items: [],
-  markers: '.uni-carrousel__container__markers__item',
+  showPrev: computed.gt('page', 0),
+  showNext: computed('page', function (page) {
+    return this.get('page') < (this.get('itemCount') - 1);
+  }),
 
-  _update() {
-    this.set('deltaX', this.get('page') * this.get('componentWidth') * -1);
-
-    if (this.get('page') === 0) {
-      $('.uni-carrousel__button--prev').hide();
-    } else {
-      $('.uni-carrousel__button--prev').show();
-    }
-
-    if (this.get('page') === this.get('itemCount') - 1) {
-      $('.uni-carrousel__button--next').hide();
-    } else {
-      $('.uni-carrousel__button--next').show();
-    }
-  },
+  deltaX: computed('page', function () {
+    return this.get('page') * this.get('componentWidth') * -1;
+  }),
 
   didRender() {
     this.set('componentWidth', $('.uni-carrousel__container').width());
-    this.set('items', $('.uni-carrousel__container__item'));
-    this._update();
   },
 
   actions: {
     navTo(index) {
       this.set('page', index);
-      this._update();
     },
     prev() {
-      if (this.get('page') > 0) {
-        this.decrementProperty('page');
-        this._update();
-      }
+      this.decrementProperty('page');
     },
     next() {
-      if (this.get('page') < this.get('items').length - 1) {
-        this.incrementProperty('page');
-        this._update();
-      }
+      this.incrementProperty('page');
     }
   }
 });
