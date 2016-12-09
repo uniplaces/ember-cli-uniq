dfacdd59d488340b024dcee769d15e6f617f54fd
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2016-12-09 10:36:21 +0000

    [RFC][NI] Add small fixes styles and logic (#56)
    * Add calendar styles and logic
    
    * Small fixes

--- addon/components/uni-datepicker.js
+++ addon/components/uni-datepicker.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 import layout from '../templates/components/uni-datepicker';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
   layout,
@@ -10,6 +10,27 @@ export default Component.extend({
   center: moment(),
   selected: moment(),
   minDate: moment(),
-  maxDate: moment().add(1, 'years'),
-  onSelect() {}
+  maxDate: moment().add(2, 'years'),
+  weekdayFormat: 'min',
+  dateFormat: 'MMMM YYYY',
+  showDaysAround: false,
+  isFirstMonth: computed('center', function() {
+    return this.get('center').isSame(this.get('minDate'), 'month');
+  }),
+  isLastMonth: computed('center', function() {
+    return this.get('center').isSame(this.get('maxDate'), 'month');
+  }),
+
+  onSelect() {},
+  onCenter() {},
+
+  actions: {
+    nextMonth() {
+      this.get('onCenter')(this.get('center').clone().add(1, 'month'));
+    },
+
+    prevMonth() {
+      this.get('onCenter')(this.get('center').clone().add(-1, 'month'));
+    }
+  }
 });
