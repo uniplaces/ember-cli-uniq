0c4fa242af699301ea691cc4e0b75d4886588d11
Miguel Melo <miguelbritomelo@gmail.com>
2017-03-02 15:53:03 +0000

    fixed missing tests

--- tests/integration/components/uni-main-photo-picker-test.js
+++ tests/integration/components/uni-main-photo-picker-test.js
@@ -6,20 +6,7 @@ moduleForComponent('uni-main-photo-picker', 'Integration | Component | uni main 
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{uni-main-photo-picker}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#uni-main-photo-picker}}
-      template block text
-    {{/uni-main-photo-picker}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
--- tests/integration/components/uni-photo-thumbnail-test.js
+++ tests/integration/components/uni-photo-thumbnail-test.js
@@ -6,20 +6,7 @@ moduleForComponent('uni-photo-thumbnail', 'Integration | Component | uni photo t
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{uni-photo-thumbnail}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#uni-photo-thumbnail}}
-      template block text
-    {{/uni-photo-thumbnail}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
