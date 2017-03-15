3fcbd2add9daadccc25f87d697f05c778a090d24
Telma Antunes <telma@uniplaces.com>
2017-03-15 17:56:44 +0000

    refactored alert, added stickyMode and close behaviour

--- addon/components/uni-alert.js
+++ addon/components/uni-alert.js
@@ -2,19 +2,25 @@ import Ember from 'ember';
 import layout from '../templates/components/uni-alert';
 import UniAlertTypes from 'ember-cli-uniq/enums/uni-alert-type';
 
-const { Component, computed  } = Ember;
+const { Component, computed, $ } = Ember;
 
 export default Component.extend({
   classNames: ['uni-alert'],
   classNameBindings: [
     'isFixed:uni-alert--fixed',
+    'stickyMode:uni-alert--sticky',
     'isSuccess:uni-alert--success',
     'isError:uni-alert--error',
-    'isWarning:uni-alert--warning'
+    'isWarning:uni-alert--warning',
+    'isClosed:uni-alert--closed'
   ],
   layout,
 
+  stickyMode: false,
   isFixed: false,
+  hasClose: false,
+  isClosed: false,
+  onClose: null,
   isSuccess: computed('type', function() {
     return this.get('type') === UniAlertTypes.SUCCESS;
   }),
@@ -23,5 +29,63 @@ export default Component.extend({
   }),
   isWarning: computed('type', function() {
     return this.get('type') === UniAlertTypes.WARNING;
-  })
+  }),
+  iconName: computed('isSuccess', 'isWarning', 'isError', function() {
+    if (this.get('isSuccess')) {
+      return 'alert--success';
+    }
+
+    if (this.get('isWarning')) {
+      return 'alert--warning';
+    }
+
+    if (this.get('isError')) {
+      return 'alert--error';
+    }
+
+    return 'alert';
+  }),
+
+  init() {
+    this._super();
+
+    if (this.get('stickyMode')) {
+      $(window).bind('scroll', () => {
+        this._verifyStickyScroll();
+      });
+    }
+  },
+
+  didRender() {
+    this._super(...arguments);
+
+    if (this.get('stickyMode')) {
+      this.set('componentTop', this.$().offset().top);
+      this._verifyStickyScroll();
+    }
+  },
+
+  actions: {
+    close() {
+      this.set('isClosed', true);
+
+      if (this.get('onClose')) {
+        this.get('onClose')();
+      }
+    }
+  },
+
+  _verifyStickyScroll() {
+    let scrollPos = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    let isBeforeCanvas = scrollPos > this.get('componentTop');
+    let isAfterCanvas = this.get('componentTop') > scrollPos + windowHeight;
+
+    this.set('isFixed', isBeforeCanvas || isAfterCanvas);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    $(window).unbind('scroll');
+  }
 });
