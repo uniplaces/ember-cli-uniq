a2bc9c28ac155b487cea81171f0417bd887c2e08
Henrique Mota <henrmota@gmail.com>
2017-03-22 18:43:45 +0000

    [RFC][UN-8400] Variable pricing on enricher frontend (#124)
    * Add on change to price input
    
    * Review improvements
    
    * Review improvements
    
    * Review improvements
    
    * Review improvements
    
    * Review improvements
    
    * Review improvements
    
    * Review improvements

--- addon/components/uni-input-price.js
+++ addon/components/uni-input-price.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/uni-input-price';
 
-const { Component, $ } = Ember;
+const { Component, $, computed } = Ember;
+
+const INPUT_TYPE_NUMBER = 'number';
 
 export default Component.extend({
   classNames: ['uni-input-price'],
   layout,
 
-  value: null,
+  value: '',
   minValue: 1,
   offset: 1,
   currency: '€',
@@ -16,16 +18,42 @@ export default Component.extend({
   maxLength: 4,
   placeholder: '0',
   isRightSideCurrency: true,
-  type: 'number',
+  type: INPUT_TYPE_NUMBER,
   options: null,
 
+  /**
+   * Converts a value to an integer when able, or leaves it as a string
+   * @public
+   */
+  numberAsInt: computed('value', {
+    get() {
+      return this.get('value');
+    },
+
+    set(_, value) {
+      if (this.get('type') !== INPUT_TYPE_NUMBER) {
+        this.set('value', value);
+
+        return value;
+      }
+
+      let newValue = isNaN(value) ? value : parseInt(value);
+      this.set('value', newValue);
+
+      return newValue;
+    }
+  }),
+
   onInvalidInput() {},
+  onChange() {},
 
   actions: {
     onChange() {
       if (!parseFloat(this.get('value')) && this.get('value') < this.get('minValue')) {
         this.get('onInvalidInput')(this.get('options'));
       }
+
+      this.get('onChange')(this.get('value'));
     },
 
     onFocusIn() {
