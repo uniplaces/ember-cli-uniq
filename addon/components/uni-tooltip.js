66065d63ea4508c55485f1796cc8aec98b770091
Telma Antunes <telma@uniplaces.com>
2017-04-26 19:37:32 +0100

    tooltip for inputs

--- addon/components/uni-tooltip.js
+++ addon/components/uni-tooltip.js
@@ -6,15 +6,18 @@ const { Component } = Ember;
 export default Component.extend({
   tagName: 'span',
   classNames: ['uni-tooltip'],
-  classNameBindings: ['isActive:uni-tooltip--active'],
+  classNameBindings: ['isActive:uni-tooltip--active', 'isAlternative:uni-tooltip--alternative'],
   layout,
 
   isInformation: false,
   isQuestion: false,
   isActive: false,
+  isAlternative: false,
 
   click() {
-    this.set('isActive', !this.get('isActive'));
+    if (!this.get('isAlternative')) {
+      this.set('isActive', !this.get('isActive'));
+    }
 
     this.get('onClick')();
   }
