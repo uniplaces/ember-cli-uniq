2080c3d68d4b432540f6e28a0599074ec6e79f27
Telma Antunes <telma@uniplaces.com>
2017-04-27 11:16:14 +0100

    more tests

--- tests/integration/components/uni-input-test.js
+++ tests/integration/components/uni-input-test.js
@@ -1,5 +1,8 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const { computed, isEmpty } = Ember;
 
 moduleForComponent('uni-input', 'Integration | Component | uni input', {
   integration: true
@@ -13,16 +16,16 @@ test('it renders', function(assert) {
   assert.equal(this.$().text().trim(), '');
 });
 
-test('it renders value and error without default error validation', function(assert) {
-  assert.expect(2);
+test('it renders error without default error validation', function(assert) {
+  assert.expect(1);
 
-  this.set('value', 'name');
-  this.set('showError', true);
+  this.set('showError', computed('value', function() {
+    return isEmpty(this.get('value'));
+  }));
   this.set('useDefaultErrorValidation', false);
 
-  this.render(hbs`{{uni-input value=value showError=showError useDefaultErrorValidation=useDefaultErrorValidation}}`);
+  this.render(hbs`{{uni-input showError=showError useDefaultErrorValidation=useDefaultErrorValidation}}`);
 
-  assert.equal(this.$('input').val(), 'name');
   assert.equal(this.$('.uni-input--error').length, 1);
 });
 
@@ -49,11 +52,20 @@ test('it renders error with default validations on empty required field', functi
 test('it renders success with default validations', function(assert) {
   assert.expect(1);
 
-  this.set('value', '');
   this.set('useDefaultSuccessValidation', true);
 
-  this.render(hbs`{{uni-input value=value useDefaultSuccessValidation=useDefaultSuccessValidation}}`);
+  this.render(hbs`{{uni-input useDefaultSuccessValidation=useDefaultSuccessValidation}}`);
   assert.equal(this.$('.uni-input--success').length, 1);
+});
+
+test('it doesnt render success with default validations and empty required field', function(assert) {
+  assert.expect(1);
+
+  this.set('isRequired', true);
+  this.set('useDefaultSuccessValidation', true);
+
+  this.render(hbs`{{uni-input isRequired=isRequired useDefaultSuccessValidation=useDefaultSuccessValidation}}`);
+  assert.equal(this.$('.uni-input--success').length, 0);
 });
 
 test('it converts numeric value', function(assert) {
