b3acaa775f57264cad8cc92865129ceca071ee4d
Alexandre Santos <alexandre.santozz@gmail.com>
2017-06-21 12:16:29 +0100

    [RFC][UN-8748] Add possibility to not send promise on uni-form onSubmit (#162)
    * Add possibility to not send promise on uni-form onSubmit
    
    * Change double to single quotes
    
    * Add tests to behaviour with and without promise
    
    * Fix code style errors

--- tests/integration/components/uni-form-test.js
+++ tests/integration/components/uni-form-test.js
@@ -1,5 +1,8 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+
+const { RSVP } = Ember;
 
 moduleForComponent('uni-form', 'Integration | Component | uni form', {
   integration: true
@@ -11,4 +14,37 @@ test('it renders', function(assert) {
   this.render(hbs`{{uni-form}}`);
 
   assert.equal(this.$().text().trim(), '');
+});
+
+test('it calls onSubmit and stops the loading after promise', function(assert) {
+  assert.expect(2);
+
+  this.set('isLoading', null);
+  this.set('submit', () => {
+    return new RSVP.Promise((resolve) => {
+      assert.equal(this.get('isLoading'), true, 'it starts the loading');
+      resolve();
+    });
+  });
+
+  this.render(hbs`{{uni-form isLoading=isLoading onSubmit=submit}}`);
+
+  this.$().find('.uni-button').click();
+
+  assert.equal(this.get('isLoading'), false, 'it ends the loading');
+});
+
+test('it calls onSubmit and works without it returning a promise', function(assert) {
+  assert.expect(2);
+
+  this.set('isLoading', null);
+  this.set('submit', () => {
+    assert.equal(this.get('isLoading'), true, 'it starts the loading');
+  });
+
+  this.render(hbs`{{uni-form isLoading=isLoading onSubmit=submit}}`);
+
+  this.$().find('.uni-button').click();
+
+  assert.equal(this.get('isLoading'), false, 'it ends the loading');
 });
