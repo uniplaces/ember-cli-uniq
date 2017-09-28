df118325abe8265b018e5bac999887f7cc4890ed
Telma Antunes <telma_ant@hotmail.com>
2017-09-28 16:20:10 +0100

    [RFC][NI] improve uni-tooltip (#196)
    * fixed tooltips on mobile, minor improvements
    
    * hide tooltip on scroll or click outside to avoid multiple tooltips open
    
    * fixes
    
    * new version from ember-reponsive

--- addon/components/uni-tooltip.js
+++ addon/components/uni-tooltip.js
@@ -1,13 +1,17 @@
 import Ember from 'ember';
 import layout from '../templates/components/uni-tooltip';
+import ClickOutsideMixin from 'ember-cli-uniq/mixins/click-outside';
 
-const { Component } = Ember;
+const { Component, $, inject: { service }, run } = Ember;
+const MARGIN_TOP = 8;
 
-export default Component.extend({
+export default Component.extend(ClickOutsideMixin, {
   tagName: 'span',
   classNames: ['uni-tooltip'],
   classNameBindings: ['isActive:uni-tooltip--active', 'isAlternative:uni-tooltip--alternative'],
   layout,
+
+  media: service(),
 
   isInformation: false,
   isQuestion: false,
@@ -17,11 +21,45 @@ export default Component.extend({
 
   onClick() {},
 
-  click() {
-    if (!this.get('isAlternative')) {
-      this.set('isActive', !this.get('isActive'));
+  onOutsideClick() {
+    if (this.get('isAlternative')) {
+      return;
     }
 
+    this._hideTooltip();
+  },
+
+  click() {
     this.get('onClick')();
+
+    if (this.get('isAlternative')) {
+      return;
+    }
+
+    if (this.get('isActive')) {
+      this._hideTooltip();
+
+      return;
+    }
+
+    this._showTooltip();
+    if (this.get('media.isMobile')) {
+      run.later(this, () => this._setTopPositionMobile(), 0);
+    }
+  },
+
+  _showTooltip() {
+    this.set('isActive', true);
+    $(window).bind('scroll.uni-tooltip', () => this._hideTooltip());
+  },
+
+  _hideTooltip() {
+    this.set('isActive', false);
+    $(window).unbind('scroll.uni-tooltip');
+  },
+
+  _setTopPositionMobile() {
+    let topPosition = this.$().offset().top - $(window).scrollTop() + this.$().height() + MARGIN_TOP;
+    this.$('.uni-tooltip__text').css('top', `${topPosition}px`);
   }
 });
