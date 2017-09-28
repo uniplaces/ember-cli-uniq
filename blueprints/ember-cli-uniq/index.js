3dd1617a6a202d11eaeafeee8cabae214f61c55a
André de Matos Joaquim <andrej_21@hotmail.com>
2017-09-28 12:09:38 +0100

    Make ember-responsive obligatory on parent app

--- blueprints/ember-cli-uniq/index.js
+++ blueprints/ember-cli-uniq/index.js
@@ -1,8 +1,12 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  beforeInstall() {
+    return this.addAddonToProject('ember-responsive');
+  },
 
   afterInstall() {
     return this.addBowerPackageToProject('uniq');
