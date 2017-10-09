f375938cf8bc1d8caded8923165dc42fd457c6c0
mmelo <mmelo@users.noreply.github.com>
2017-10-09 10:52:58 +0100

    change progressbar layout (#200)

--- tests/integration/components/uni-progress-bar-test.js
+++ tests/integration/components/uni-progress-bar-test.js
@@ -12,3 +12,13 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), '');
 });
+
+test('it renders step markers', function(assert) {
+  assert.expect(1);
+
+  this.set('steps', 6);
+
+  this.render(hbs`{{uni-progress-bar totalStepsCount=steps}}`);
+
+  assert.equal(this.$().find('.uni-progress-bar__step').length - 1, this.get('steps'));
+});
---
+++ tests/unit/utils/array-methods-test.js
@@ -0,0 +1,14 @@
+import arrayMethods from 'dummy/utils/array-methods';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | array methods');
+
+test('it works', function(assert) {
+  assert.expect(2);
+
+  let arraySize = 5;
+  let result = arrayMethods(0, arraySize);
+
+  assert.ok(result);
+  assert.equal(result.length - 1, arraySize);
+});
