b86abe9c3cd9298c1e698559769554730b03a9df
DaniFrancisco <daniel.francisco@uniplaces.com>
2017-10-23 14:23:16 +0100

    Review improvement

--- addon/utils/is-valid-phone-number.js
+++ addon/utils/is-valid-phone-number.js
@@ -1,11 +1,5 @@
-import CountryCodes from 'ember-cli-uniq/enums/country-codes-type';
 import PhoneNumberValidations from 'ember-cli-uniq/enums/phone-number-validation-type';
 
 export default function isValidPhoneNumber(countryCode, number) {
-  switch (countryCode) {
-    case CountryCodes.PT:
-      return PhoneNumberValidations.PT.test(number);
-    default:
-      return true;
-  }
+  return PhoneNumberValidations[countryCode] ? PhoneNumberValidations[countryCode].test(number) : true;
 }
