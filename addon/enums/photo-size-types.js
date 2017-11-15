bbee6828b1bce060a1481c8489e6f6ffecef7c47
Alexandre Santos <alexandre.santos@uniplaces.com>
2017-11-15 13:58:37 +0000

    Add photo-size-types enum

---
+++ addon/enums/photo-size-types.js
@@ -0,0 +1,10 @@
+import EmberObject from 'ember-object';
+
+export default EmberObject.create({
+  ORIGINAL: 'original',
+  XX_LARGE: 'xx-large',
+  X_LARGE: 'x-large',
+  LARGE: 'large',
+  MEDIUM: 'medium',
+  SMALL: 'small'
+});
