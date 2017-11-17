3e049d4c6d20af0ee369020990ee57ddda14446b
André Joaquim <andrej_21@hotmail.com>
2017-11-17 11:49:20 +0000

    Review improvements

--- tests/dummy/app/components/demo-page.js
+++ tests/dummy/app/components/demo-page.js
@@ -2,29 +2,43 @@ import Component from '@ember/component';
 import $ from 'jquery';
 
 const DEVILS_NUMBER = 666 / 2; // Give this a proper name  (╯°□°）╯︵ ┻━┻
+const ANIMATION_DURATION = 300;
 
 export default Component.extend({
+
+  options: {
+    anchorSelector: 'a[href^="#"]',
+    componentsSelector: '.components',
+    topBannerSelector: '.demo-app__banner',
+    containerSelector: '.container',
+    topBannerSmallerClass: 'demo-app__banner--smaller',
+    containerBiggerClass: 'container--bigger'
+  },
 
   didRender() {
     this._super(...arguments);
 
-    $(document).on('click', 'a[href^="#"]', function(event) {
+    let anchors = this.get('options.anchorSelector');
+    $(document).on('click', anchors, function() {
       event.preventDefault();
 
       let componentsContainer = $('.components');
       componentsContainer.animate({
         scrollTop: componentsContainer.scrollTop() + $($.attr(this, 'href')).position().top
-      }, 500);
+      }, ANIMATION_DURATION);
     });
 
-    $('.components').scroll(function() {
-      if ($('.components').scrollTop() > DEVILS_NUMBER) {
-        $('.demo-app__banner').addClass('demo-app__banner--smaller');
-        $('.container').addClass('container--bigger');
-      } else {
-        $('.demo-app__banner').removeClass('demo-app__banner--smaller');
-        $('.container').removeClass('container--bigger');
-      }
-    });
+    $('.components').scroll(this.handleScroll.bind(this));
+  },
+
+  handleScroll() {
+    let demoAppBanner = $(this.get('options.topBannerSelector'));
+    let outerContainer = $(this.get('options.containerSelector'));
+    let topBannerSmallerClass = this.get('options.topBannerSmallerClass');
+    let containerBiggerClass = this.get('options.containerBiggerClass');
+
+    let action = $(this.get('options.componentsSelector')).scrollTop() > DEVILS_NUMBER ? 'add' : 'remove';
+    demoAppBanner[`${action}Class`](topBannerSmallerClass);
+    outerContainer[`${action}Class`](containerBiggerClass);
   }
 });
