032be02f82e17a04266b5bf66366c6cb40a30c07
sardyy <tiagosardinha94@hotmail.com>
2018-01-10 16:58:44 +0000

    Fix tests (#217)

--- tests/integration/components/uni-button-dropdown-item-test.js
+++ tests/integration/components/uni-button-dropdown-item-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { click } from 'ember-native-dom-helpers';
 
 moduleForComponent('uni-button-dropdown-item', 'Integration | Component | uni button dropdown item', {
   integration: true
@@ -11,14 +12,17 @@ test('it renders', function(assert) {
   let expectedValue = 'Portugal';
   let expectedDescription = 'Nice country';
   this.set('option', { key: 'pt', value: expectedValue, description: expectedDescription });
+  this.set('onClick', (key) => {
+    return key;
+  });
 
-  this.render(hbs`{{uni-button-dropdown-item option=option}}`);
+  this.render(hbs`{{uni-button-dropdown-item onClick=onClick option=option}}`);
 
   assert.equal(this.$('.uni-button-dropdown__list__item--value').text().trim(), expectedValue);
   assert.equal(this.$('.uni-button-dropdown__list__item--description').text().trim(), expectedDescription);
 });
 
-test('it executes action when clicked', function(assert) {
+test('it executes action when clicked', async function(assert) {
   assert.expect(1);
 
   let expectedKey = 'pt';
@@ -30,5 +34,6 @@ test('it executes action when clicked', function(assert) {
   this.set('option', { key: expectedKey, value: 'Portugal', description: 'Nice country' });
 
   this.render(hbs`{{uni-button-dropdown-item option=option onClick=clickAction}}`);
-  this.$().click();
+
+  await click('.uni-button-dropdown__list__item');
 });
