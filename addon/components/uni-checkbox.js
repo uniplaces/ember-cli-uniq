d00ff48936745b1e53303cd6267202d9afe2ab65
Alexandre Santos <alexandre.santos@uniplaces.com>
2018-01-18 10:54:12 +0000

    Add small checkbox

--- addon/components/uni-checkbox.js
+++ addon/components/uni-checkbox.js
@@ -4,6 +4,7 @@ import layout from '../templates/components/uni-checkbox';
 export default Component.extend({
   layout,
 
+  isSmall: false,
   isSelected: false,
   isDisabled: false,
   hasError: false,
