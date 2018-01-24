52cbba52eeba093ae34045895117245d3e86bbec
Alexandre Santos <alexandre.santos@uniplaces.com>
2018-01-24 18:28:28 +0000

    Add uni-datepicker-range-day

---
+++ addon/components/uni-datepicker-range.js
@@ -0,0 +1,36 @@
+import Component from '@ember/component';
+import layout from '../templates/components/uni-datepicker-range';
+import moment from 'moment';
+
+export default Component.extend({
+  layout,
+  classNames: ['uni-datepicker-range'],
+
+  center: moment(),
+  minDate: moment(),
+  maxDate: moment().add(3, 'years'),
+  weekdayFormat: 'min',
+  titleFormat: 'MMMM YYYY',
+  showDaysAround: true,
+  disabledDates: [],
+  highlightedDates: [],
+  minimumStay: null,
+
+  onSelect() {},
+  onCenter() {},
+  onMouseOver() {},
+  onMouseOut() {},
+  onDisabledClick() {},
+
+  actions: {
+    nextMonth() {
+      this.set('center', this.get('center').clone().add(1, 'months'));
+      this.get('onCenter')(this.get('center'));
+    },
+
+    previousMonth() {
+      this.set('center', this.get('center').clone().subtract(1, 'months'));
+      this.get('onCenter')(this.get('center'));
+    }
+  }
+});
