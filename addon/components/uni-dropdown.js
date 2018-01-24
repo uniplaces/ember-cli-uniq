2aebab93238f34fcbe22feb6c8d252084f071b3c
mmelo <mmelo@users.noreply.github.com>
2018-01-24 15:40:01 +0000

    [RFC][UN-11823] Improve calendars and dropdowns in mobile (#227)
    * add on-demand native dropdown behaviour to uni-dropdown
    
    * add requested fixes
    
    * cause im an idiot

--- addon/components/uni-dropdown.js
+++ addon/components/uni-dropdown.js
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import layout from '../templates/components/uni-dropdown';
 import ClickOutsideMixin from 'ember-cli-uniq/mixins/click-outside';
 
 export default Component.extend(ClickOutsideMixin, {
+  media: service(),
+
   classNames: ['uni-dropdown'],
   classNameBindings: [
     'isOpen:uni-dropdown--active',
@@ -12,6 +15,7 @@ export default Component.extend(ClickOutsideMixin, {
   ],
   layout,
 
+  useAlias: false,
   selectedAlias: null,
   selectedSvgs: null,
   isOpen: false,
