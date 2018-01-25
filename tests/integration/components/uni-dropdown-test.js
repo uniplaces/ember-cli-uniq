579a865346f66c87e83be90a5bee3e073cc05381
Miguel Melo <miguelbritomelo@gmail.com>
2018-01-25 15:58:39 +0000

    remove unnecessary tests

--- tests/integration/components/uni-dropdown-test.js
+++ tests/integration/components/uni-dropdown-test.js
@@ -1,6 +1,4 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import { setBreakpointForIntegrationTest } from '../../helpers/responsive';
-import { find } from 'ember-native-dom-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('uni-dropdown', 'Integration | Component | uni dropdown', {
@@ -44,21 +42,4 @@ test('it renders given svg', function(assert) {
 
   assert.equal(this.$('.uni-dropdown__button').text().trim(), 'google');
   assert.equal(this.$('.uni-dropdown__button .uni-dropdown__svg-group svg').length, 1);
-});
-
-test('it renders native select on mobile devices', function(assert) {
-  assert.expect(1);
-
-  setBreakpointForIntegrationTest(this, 'mobile');
-
-  this.set('options', [
-    { key: 'facebook', value: 'Facebook' },
-    { key: 'google', value: 'Google' }
-  ]);
-  this.set('selected', 'facebook');
-  this.set('selectedAlias', 'example');
-
-  this.render(hbs`{{uni-dropdown options=options selected=selected selectedAlias=selectedAlias media=media}}`);
-
-  assert.ok(find('select'));
 });
