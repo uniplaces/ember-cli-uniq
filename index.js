c8954042d3637e59ee19885c8723d1d628fd4288
Alexandre Santos <alexandre.santos@uniplaces.com>
2018-04-02 17:10:54 +0100

    Add treeForPublic to exclude flags from build

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 /* eslint-env node */
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-cli-uniq'
+  name: 'ember-cli-uniq',
+
+  included() {
+    this._super.included && this._super.included.apply(this, arguments);
+
+    let addonOptions = (this.parent && this.parent.options) || (this.app && this.app.options) || {};
+    let config = addonOptions[this.name] || {};
+
+    this.excludeFlags = config.excludeFlags;
+  },
+
+  treeForPublic() {
+    let tree = this._super.treeForPublic.apply(this, arguments);
+
+    if (this.excludeFlags) {
+      return new Funnel(tree, {
+        exclude: ['public/assets/images/flags']
+      });
+    }
+
+    return tree;
+  }
 };
