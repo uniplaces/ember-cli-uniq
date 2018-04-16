c34220782bc796a293633cdd9b1fd73eb06fd573
DaniFrancisco <daniel.francisco@uniplaces.com>
2018-04-16 11:10:16 +0100

    Review improvements

--- tests/integration/components/uni-tooltip-test.js
+++ tests/integration/components/uni-tooltip-test.js
@@ -1,20 +1,17 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { click, find } from 'ember-native-dom-helpers';
-import getOwner from 'ember-owner/get';
+import { setBreakpointForIntegrationTest } from '../../helpers/responsive';
 
 moduleForComponent('uni-tooltip', 'Integration | Component | uni tooltip', {
-  integration: true,
-  beforeEach() {
-    let mediaService = getOwner(this).lookup('service:media');
-    mediaService.set('isMobile', true);
-  }
+  integration: true
 });
 
 test('it renders', function(assert) {
   assert.expect(3);
 
-  this.render(hbs`{{uni-tooltip}}`);
+  setBreakpointForIntegrationTest(this, 'mobile');
+  this.render(hbs`{{uni-tooltip media=media}}`);
 
   assert.equal(this.$().text().trim(), '');
   assert.notOk(find('.uni-tooltip__text'));
