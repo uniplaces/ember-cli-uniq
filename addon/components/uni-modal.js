091a79e7bd0d05e410d6ed82d843f43ee54e3173
Tiago <tiagosardinha94@hotmail.com>
2018-05-14 15:30:58 +0100

    Review improvements

--- addon/components/uni-modal.js
+++ addon/components/uni-modal.js
@@ -3,30 +3,32 @@ import { observer } from '@ember/object';
 import $ from 'jquery';
 import layout from '../templates/components/uni-modal';
 
-const OVERFLOW_HIDDEN_CLASS = 'overflow-hidden';
-
 export default Component.extend({
   tagName: '',
   layout,
   baseCssClass: 'uni-modal',
   modifierCssClass: '',
-
-  title: null,
   customCssComponentClass: '',
+  bodyOverflowClass: 'overflow-hidden',
+  title: null,
   hasSeparator: true,
   hasCloseButton: true,
   renderInPlace: false,
   isOpen: null,
+
   onCloseModal() {},
 
   // This observer is used to bypass the scroll on mobile when a modal is open
   onOpenChangeObserver: observer('isOpen', function() {
-    this.get('isOpen') ? $('body').addClass(OVERFLOW_HIDDEN_CLASS) : $('body').removeClass(OVERFLOW_HIDDEN_CLASS);
+    this.get('isOpen')
+      ? $('body').addClass(this.get('bodyOverflowClass'))
+      : $('body').removeClass(this.get('bodyOverflowClass'));
   }),
 
   didDestroyElement() {
     this._super(...arguments);
-    $('body').removeClass(OVERFLOW_HIDDEN_CLASS);
+
+    $('body').removeClass(this.get('bodyOverflowClass'));
   },
 
   actions: {
