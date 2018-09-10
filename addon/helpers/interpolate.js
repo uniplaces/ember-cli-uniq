88650732781cdacf353f448e6de951bfb9ef4ba7
André Joaquim <andrej_21@hotmail.com>
2018-09-10 11:20:08 +0100

    Remove unnecessary variable

--- addon/helpers/interpolate.js
+++ addon/helpers/interpolate.js
@@ -2,9 +2,7 @@ import { helper } from '@ember/component/helper';
 import { get } from '@ember/object';
 
 export function interpolate([str], hash) {
-  const regularExpression = /\$\{\s*(.*?)\s*\}/g;
-
-  return str.replace(regularExpression, (i, match) => get(hash, match));
+  return str.replace(/\$\{\s*(.*?)\s*\}/g, (i, match) => get(hash, match));
 }
 
 export default helper(interpolate);
