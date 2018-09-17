38a324af26f2076523a9d1412e99d2b692aa84bc
André de Matos Joaquim <andrej_21@hotmail.com>
2018-09-17 17:11:45 +0100

    Add yield option to uni-button's label (#272)

--- tests/integration/components/uni-button-test.js
+++ tests/integration/components/uni-button-test.js
@@ -8,7 +8,26 @@ moduleForComponent('uni-button', 'Integration | Component | uni button', {
 test('it renders', function(assert) {
   assert.expect(1);
 
-  this.render(hbs`{{uni-button}}`);
+  this.set('label', 'This is a label');
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{uni-button label=label}}`);
+
+  assert.dom('.uni-button').hasText('This is a label');
+});
+
+test('it renders yield content', function(assert) {
+  assert.expect(4);
+
+  this.set('label', 'This label should not show');
+
+  this.render(hbs`
+    {{#uni-button label=label}}
+      <h1>Click on me!</h1>
+    {{/uni-button}}
+  `);
+
+  assert.dom('.uni-button').exists();
+  assert.dom('.uni-button').doesNotIncludeText('This label should not show');
+  assert.dom('h1').exists();
+  assert.dom('h1').hasText('Click on me!');
 });
