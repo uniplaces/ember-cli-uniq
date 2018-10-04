4dc052323635413cc630fa98d9ab9bf88d594f2e
André Joaquim <andrej_21@hotmail.com>
2018-10-04 18:41:16 +0100

    Add transform opt in to-money helper

--- addon/helpers/to-money.js
+++ addon/helpers/to-money.js
@@ -1,5 +1,5 @@
-import { isEmpty } from '@ember/utils';
 import Helper from '@ember/component/helper';
+import { isEmpty, isPresent } from '@ember/utils';
 import { inject as service } from '@ember/service';
 import toMoney from 'ember-cli-uniq/utils/to-money';
 
@@ -8,16 +8,15 @@ const DEFAULT_LOCALE = 'en-gb';
 export default Helper.extend({
   i18n: service('i18n'),
 
-  compute([params]) {
-    let i18n = this.get('i18n');
-
+  compute([params], namedArgs) {
     if (isEmpty(params)) {
       return '';
     }
 
-    let locale = i18n.get('locale') || DEFAULT_LOCALE;
-    let { amount, currency_code } = params;
+    const locale = this.get('i18n.locale') || DEFAULT_LOCALE;
+    const { amount, currency_code } = params;
+    const transformedAmount = isPresent(namedArgs.transform) && !namedArgs.transform ? amount * 100 : amount;
 
-    return toMoney(amount, currency_code, locale);
+    return toMoney(transformedAmount, currency_code, locale);
   }
 });
