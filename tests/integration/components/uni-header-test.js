58eb3781ef3a04ff5fc56f3bfc3beff5971c95f4
mmelo <mmelo@users.noreply.github.com>
2018-10-17 13:57:29 +0100

    [RFC][UN-14831] Allow custom header logo (#278)
    * add custom logo functionality to header component
    
    * added better tests

--- tests/integration/components/uni-header-test.js
+++ tests/integration/components/uni-header-test.js
@@ -12,3 +12,19 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), '');
 });
+
+test('it renders with a custom logo', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    logoSrc: 'https://www.ulisboa.pt/sites/ulisboa.pt/files/styles/logos_80px_vert/public/uo/logos/logo_ist.jpg',
+    logoHeight: 60,
+    logoWidth: 'auto'
+  });
+
+  this.render(hbs`{{uni-header logoSrc=logoSrc logoHeight=logoHeight logoWidth=logoWidth}}`);
+
+  let img = `img[src="${this.get('logoSrc')}"]`;
+
+  assert.dom(img).exists();
+});
