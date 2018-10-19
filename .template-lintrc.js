30acbfbc8dd88b1b0d0ccf436936702c61beca64
Miguel Moura <miguel.moura@uniplaces.com>
2018-10-19 19:08:34 +0100

    Update ember-cli to 3.5

---
+++ .template-lintrc.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'no-bare-strings': false,
+    'attribute-indentation': {
+      'mustache-open-end': 'last-attribute',
+      'open-invocation-max-len': 120
+    },
+    // @TODO: Remove the below rules
+    'no-invalid-interactive': false,
+    'no-triple-curlies': false,
+    'no-inline-styles': false
+  }
+};
