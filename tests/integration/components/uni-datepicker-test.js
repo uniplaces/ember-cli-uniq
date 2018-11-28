2d2bd386366687b46458df223a99f7d290370726
mario <mario@uniplaces.com>
2018-11-28 15:56:11 +0000

    add new coding standards to tests

--- tests/integration/components/uni-datepicker-test.js
+++ tests/integration/components/uni-datepicker-test.js
@@ -1,58 +1,60 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'ember-qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import MessageType from 'ember-cli-uniq/enums/uni-datepicker-message-type';
 
-moduleForComponent('uni-datepicker', 'Integration | Component | uni datepicker', {
-  integration: true
-});
+module('Integration | Component | uni datepicker', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders the first warning message', function(assert) {
-  this.set('messages', [
-    { type: MessageType.INFORMATION, label: 'This is a information label' },
-    { type: MessageType.WARNING, label: 'This is a warning label 1' },
-    { type: MessageType.WARNING, label: 'This is a warning label 2' }
-  ]);
+  test('it renders the first warning message', async function(assert) {
+    this.set('messages', [
+      { type: MessageType.INFORMATION, label: 'This is a information label' },
+      { type: MessageType.WARNING, label: 'This is a warning label 1' },
+      { type: MessageType.WARNING, label: 'This is a warning label 2' }
+    ]);
 
-  this.render(hbs`{{uni-datepicker messages=messages}}`);
+    await render(hbs`{{uni-datepicker messages=messages}}`);
 
-  assert.dom('p').hasText('This is a warning label 1');
-});
+    assert.dom('p').hasText('This is a warning label 1');
+  });
 
-test('it renders the first information label when there is no warning message present', function(assert) {
-  this.set('messages', [
-    { type: MessageType.INFORMATION, label: 'This is a information label' },
-    { type: MessageType.INFORMATION, label: 'This is a information label 2' },
-    { type: MessageType.INFORMATION, label: 'This is a information label 3' }
-  ]);
+  test('it renders the first information label when there is no warning message present', async function(assert) {
+    this.set('messages', [
+      { type: MessageType.INFORMATION, label: 'This is a information label' },
+      { type: MessageType.INFORMATION, label: 'This is a information label 2' },
+      { type: MessageType.INFORMATION, label: 'This is a information label 3' }
+    ]);
 
-  this.render(hbs`{{uni-datepicker messages=messages}}`);
+    await render(hbs`{{uni-datepicker messages=messages}}`);
 
-  assert.dom('p').hasText('This is a information label');
-});
+    assert.dom('p').hasText('This is a information label');
+  });
 
-test('it renders the first warning message', function(assert) {
-  this.set('messages', [
-    { type: MessageType.WARNING, label: 'This is a warning label' },
-    { type: MessageType.WARNING, label: 'This is a warning label 2' },
-    { type: MessageType.WARNING, label: 'This is a warning label 3' }
-  ]);
+  test('it renders the first warning message', async function(assert) {
+    this.set('messages', [
+      { type: MessageType.WARNING, label: 'This is a warning label' },
+      { type: MessageType.WARNING, label: 'This is a warning label 2' },
+      { type: MessageType.WARNING, label: 'This is a warning label 3' }
+    ]);
 
-  this.render(hbs`{{uni-datepicker messages=messages}}`);
+    await render(hbs`{{uni-datepicker messages=messages}}`);
 
-  assert.dom('p').hasText('This is a warning label');
-});
+    assert.dom('p').hasText('This is a warning label');
+  });
 
-test('when there is no messages the message container does not render', function(assert) {
-  this.set('messages', []);
+  test('when there is no messages the message container does not render', async function(assert) {
+    this.set('messages', []);
 
-  this.render(hbs`{{uni-datepicker messages=messages}}`);
+    await render(hbs`{{uni-datepicker messages=messages}}`);
 
-  assert.dom('.message').doesNotExist();
-});
+    assert.dom('.message').doesNotExist();
+  });
 
-test('test to see if the modal went fullscreen', function(assert) {
+  test('test to see if the modal went fullscreen', async function(assert) {
 
-  this.render(hbs`{{uni-datepicker isFullScreen=true}}`);
+    await render(hbs`{{uni-datepicker isFullScreen=true}}`);
 
-  assert.dom('.fullscreen').exists();
+    assert.dom('.fullscreen').exists();
+  });
 });
