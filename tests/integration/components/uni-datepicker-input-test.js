5f05f2f03e23aac3ec8427b34bbc052573f6d7c2
Mário Pereira <mario.pedro@niceoutput.com>
2018-11-28 16:23:18 +0000

    add new coding standards to tests (#297)

--- tests/integration/components/uni-datepicker-input-test.js
+++ tests/integration/components/uni-datepicker-input-test.js
@@ -1,29 +1,31 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'ember-qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { click } from 'ember-native-dom-helpers';
 
-moduleForComponent('uni-datepicker-input', 'Integration | Component | uni datepicker input', {
-  integration: true
-});
+module('Integration | Component | uni datepicker input', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  assert.expect(1);
+  test('it renders', async function(assert) {
+    assert.expect(1);
 
-  this.render(hbs`{{uni-datepicker-input}}`);
+    await render(hbs`{{uni-datepicker-input}}`);
 
-  assert.dom('.uni-datepicker-input').exists();
-});
+    assert.dom('.uni-datepicker-input').exists();
+  });
 
-test('it toggles date picker component existence when clicking input', async function(assert) {
-  assert.expect(2);
+  test('it toggles date picker component existence when clicking input', async function(assert) {
+    assert.expect(2);
 
-  this.render(hbs`{{uni-datepicker-input}}`);
+    await render(hbs`{{uni-datepicker-input}}`);
 
-  await click('.uni-input');
+    await click('.uni-input');
 
-  assert.dom('.uni-datepicker').exists();
+    assert.dom('.uni-datepicker').exists();
 
-  await click('.uni-input');
+    await click('.uni-input');
 
-  assert.dom('.uni-datepicker').doesNotExist();
+    assert.dom('.uni-datepicker').doesNotExist();
+  });
 });
