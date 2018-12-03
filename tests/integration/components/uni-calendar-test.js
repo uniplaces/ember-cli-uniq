f1db33c3f30e0c460a2b3ed02a8ad0f85eb178c6
mario <mario@uniplaces.com>
2018-12-03 12:24:14 +0000

    fix tests according code reviews

--- tests/integration/components/uni-calendar-test.js
+++ tests/integration/components/uni-calendar-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | uni calendar', function(hooks) {
@@ -12,5 +12,19 @@ module('Integration | Component | uni calendar', function(hooks) {
     await render(hbs`{{uni-calendar}}`);
 
     assert.dom('.uni-datepicker-input__wrapper').exists({ count: 2 });
+  });
+
+  test('it renders datepicker after click and closes after clicking on calendar', async function(assert) {
+    assert.expect(2);
+
+    await render(hbs`{{uni-calendar showDatepicker=true}}`);
+
+    await click('.uni-input');
+
+    assert.dom('.uni-datepicker').exists();
+
+    await click('.ember-power-calendar-day');
+
+    assert.dom('.uni-datepicker').doesNotExist();
   });
 });
