c5f47cc4b9e3658839d5476e96ce326cef031e70
mario <mario@uniplaces.com>
2018-12-03 11:12:55 +0000

    fix uni-calendar tests

--- tests/integration/components/uni-calendar-test.js
+++ tests/integration/components/uni-calendar-test.js
@@ -1,24 +1,16 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('uni-calendar', 'Integration | Component | uni calendar', {
-  integration: true
-});
+module('Integration | Component | uni calendar', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders both inputs', async function(assert) {
+    assert.expect(1);
 
-  this.render(hbs`{{uni-calendar}}`);
+    await render(hbs`{{uni-calendar}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#uni-calendar}}
-      template block text
-    {{/uni-calendar}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.dom('.uni-datepicker-input__wrapper').exists({ count: 2 });
+  });
 });
