66685721441ee53cad619834ad47eccf327042d3
Miguel Moura <miguel.moura@uniplaces.com>
2018-12-10 17:24:59 +0000

    Add test

--- tests/integration/components/uni-checkbox-test.js
+++ tests/integration/components/uni-checkbox-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | uni checkbox', function(hooks) {
@@ -19,6 +19,14 @@ module('Integration | Component | uni checkbox', function(hooks) {
 
     await render(hbs`{{uni-checkbox isSmall=true}}`);
 
-    assert.ok(find('.uni-checkbox').getAttribute('class').includes('--small'));
+    assert.dom('.uni-checkbox').hasClass('uni-checkbox--small');
+  });
+
+  test('it renders icon', async function(assert) {
+    assert.expect(1);
+
+    await render(hbs`{{uni-checkbox icon="cross"}}`);
+
+    assert.dom('.icon').exists();
   });
 });
