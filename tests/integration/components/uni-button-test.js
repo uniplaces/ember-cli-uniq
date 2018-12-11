4fb5eb51cda78a1a287440df8f767bf3bbdde9b9
Miguel Moura <miguel.moura@uniplaces.com>
2018-12-11 18:29:53 +0000

    Add test

--- tests/integration/components/uni-button-test.js
+++ tests/integration/components/uni-button-test.js
@@ -13,11 +13,11 @@ module('Integration | Component | uni button', function(hooks) {
 
     await render(hbs`{{uni-button label=label}}`);
 
-    assert.dom('.uni-button').hasText('This is a label');
+    assert.dom('.uni-button .label').hasText('This is a label');
   });
 
   test('it renders yield content', async function(assert) {
-    assert.expect(4);
+    assert.expect(5);
 
     this.set('label', 'This label should not show');
 
@@ -28,8 +28,22 @@ module('Integration | Component | uni button', function(hooks) {
     `);
 
     assert.dom('.uni-button').exists();
-    assert.dom('.uni-button').doesNotIncludeText('This label should not show');
+    assert.dom('.uni-button .label').doesNotExist();
+    assert.dom('.uni-button .icon').doesNotExist();
     assert.dom('h1').exists();
     assert.dom('h1').hasText('Click on me!');
+  });
+
+  test('it renders icon', async function(assert) {
+    assert.expect(1);
+
+    this.setProperties({
+      icon: 'check',
+      label: 'This is a label'
+    });
+
+    await render(hbs`{{uni-button label=label icon=icon}}`);
+
+    assert.dom('.uni-button .icon').exists();
   });
 });
