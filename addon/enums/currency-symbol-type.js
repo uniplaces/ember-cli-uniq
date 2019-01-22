2c29aecadb5e2c4731c96ed9202b4d2bb67e120e
Renato Duarte <renato.duarte@uniplaces.com>
2019-01-22 14:27:07 +0000

    Revert delete of enum, in order to minimize breaking changes. [Code review]

---
+++ addon/enums/currency-symbol-type.js
@@ -0,0 +1,11 @@
+import Enumerable from './enumerable';
+
+/**
+ * @deprecated since version 0.4.14. Please use currency-type instead.
+ */
+export default Enumerable.create({
+  EUR: '€',
+  GBP: '£',
+  USD: '$',
+  SEK: 'kr'
+});
