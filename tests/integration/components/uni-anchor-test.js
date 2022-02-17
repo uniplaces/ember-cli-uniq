c4fae6ba135940e0819d8fadd5b5de71f87df068
João Madeira <joaomadeira@outlook.pt>
2022-02-17 07:19:45 +0000

    [RFC][SUP-379] Add rel attribute binding on uni-anchor component (#322)
    * Added rel attribute binding on uni-anchor
    
    * Uni-anchor test updated

--- tests/integration/components/uni-anchor-test.js
+++ tests/integration/components/uni-anchor-test.js
@@ -4,18 +4,21 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 const DEFAULT_LABEL = 'This is a label';
+const DEFAULT_REL = 'noopener noreferrer';
 
 module('Integration | Component | uni anchor', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    assert.expect(2);
+    assert.expect(3);
 
     this.set('label', DEFAULT_LABEL);
+    this.set('rel', DEFAULT_REL);
 
-    await render(hbs`{{uni-anchor label=label}}`);
+    await render(hbs`{{uni-anchor label=label rel=rel}}`);
 
     assert.dom('.uni-anchor').exists();
     assert.dom('.uni-anchor').hasText(DEFAULT_LABEL);
+    assert.dom('.uni-anchor').hasAttribute('rel', DEFAULT_REL);
   });
 });
